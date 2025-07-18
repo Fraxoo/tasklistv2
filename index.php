001931e24df6fe56fe3637ec0f94c9620b22adf8
Fraxoo <fraxo.officiel@gmail.com>
2025-07-18 13:26:18 +0200

    deco

--- index.php
+++ index.php
@@ -1,35 +1,27 @@
-<?php 
+<?php
 session_start();
 
 $user = "root";
 $pass = "root";
 
-try
-
-{
-$bdd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=app-database', $user, $pass);
-}
-
-catch (Exception $e)
-
-{
+try {
+    $bdd = new PDO('mysql:host=127.0.0.1;port=3306;dbname=app-database', $user, $pass);
+} catch (Exception $e) {
 
     die('Erreur : ' . $e->getMessage());
-
 }
 
 $acount_id = 0;
 
 $tache = $_POST['tache'];
 
-if(isset($tache)){
+if (isset($tache)) {
 
     $addtaskbdd = $bdd->prepare('INSERT INTO task(task,acount_id) VALUES (:task,:acount_id)');
     $addtaskbdd->execute([
         'task' => $tache,
-        'acount_id'=> $acount_id
+        'acount_id' => $acount_id
     ]);
-
 }
 
 
@@ -41,6 +33,7 @@ if(isset($tache)){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -53,37 +46,42 @@ if(isset($tache)){
 
 
 <body>
-<?php if(!isset($_SESSION['id'])):?>
+    <?php if (!isset($_SESSION['id'])): ?>
 
-<div class="top">
-    <a href="register.php">S'inscrire</a>
-    <a href="login.php">Se connecter</a>
-</div>
-<?php else: ?>
-<div class="nom">
-    <p>Bonjour <?= $_SESSION['prenom']?></p>
-</div>
-<?php endif ?>
-<div class="main">
+        <div class="top">
+            <a href="register.php">S'inscrire</a>
+            <a href="login.php">Se connecter</a>
+        </div>
+    <?php else: ?>
+        <div class="top2">
+            <div class="nom">
+                <p>Bonjour <?= $_SESSION['prenom'] ?></p>
+            </div>
+            <div class="deco">
+                <a href="logout.php">Se Deconecter</a>
+            </div>
+        </div>
+    <?php endif ?>
+    <div class="main">
 
-    <div class="titre">
-        <p>MA</p>
-        <h1>To Do List</h1> 
+        <div class="titre">
+            <p>MA</p>
+            <h1>To Do List</h1>
+        </div>
+
+        <div class="add">
+            <form action="/" method="post">
+                <input type="text" name="tache" placeholder="Ajoutez une tache :" required>
+                <button>Ajoutez tache</button>
+            </form>
+        </div>
+
+        <div class="remove">
+            <button>Supprimer Tache</button>
+        </div>
+
+
     </div>
-
-    <div class="add">
-        <form action="/" method="post">
-            <input type="text" name="tache" placeholder="Ajoutez une tache :" required>
-            <button>Ajoutez tache</button>
-        </form>
-    </div>
-
-    <div class="remove">
-        <button>Supprimer Tache</button>
-    </div>
-
-
-</div>
 
 </body>
 
